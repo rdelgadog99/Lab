e9488b95d5ad821aba69611b83116f4a58574d32
rdelgadog99 <111252209+rdelgadog99@users.noreply.github.com>
2022-10-03 21:29:28 -0500

    Add files via upload

---
+++ LB5.X/build/default/production/knight_rider.p1.d
@@ -0,0 +1,5 @@
+build/default/production/knight_rider.p1:  \
+knight_rider.c  \
+mcc_generated_files/mcc.h  \
+mcc_generated_files/device_config.h  \
+mcc_generated_files/pin_manager.h 
---
+++ LB5.X/build/default/production/mustang.p1.d
@@ -0,0 +1,5 @@
+build/default/production/mustang.p1:  \
+mustang.c  \
+mcc_generated_files/mcc.h  \
+mcc_generated_files/device_config.h  \
+mcc_generated_files/pin_manager.h 
